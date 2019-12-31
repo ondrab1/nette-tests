1ab6c59504b28cfea31b306e2e58a4ecc0d4967e
Ondrej Benes <ondrab97@gmail.com>
2019-12-31 16:51:49 +0100

    use dbunit

--- src/NetteTests/TestCases/DIContainerTestCase.php
+++ src/NetteTests/TestCases/DIContainerTestCase.php
@@ -11,10 +11,35 @@ abstract class DIContainerTestCase extends TestCase
 
 	use SetupContainer;
 
+	private $connection;
+
 	protected function setUp(): void
 	{
 		parent::setUp();
 		$this->setupContainer(Configuration::createConfigurator(), $this);
 	}
+
+	protected function getConnection()
+    {
+        if (!isset($_ENV['NETTE__DB_HOST']) || !isset($_ENV['NETTE__DB_NAME']) || !isset($_ENV['NETTE__DB_USERNAME'])
+            || !isset($_ENV['NETTE__DB_PASSWORD'])) {
+            throw new \Exception('Missing database env variables');
+        }
+
+        if (!$this->connection) {
+            $dsn = 'mysql:dbname='.$_ENV['NETTE__DB_NAME'].';host='.$_ENV['NETTE__DB_HOST'];
+            $pdo = new \PDO($dsn, $_ENV['NETTE__DB_USERNAME'], $_ENV['NETTE__DB_PASSWORD']);
+
+            $this->connection = $this->createDefaultDBConnection($pdo);
+        }
+
+
+        return $this->connection;
+    }
+
+    protected function getDataSet()
+    {
+        return $this->createArrayDataSet([]);
+    }
 
 }
